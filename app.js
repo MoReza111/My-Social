dc491be4e7d3adbd193d6e45521b2f69d7c592fc
MoReza111 <mz.abdollahi223@gmail.com>
2021-06-04 17:05:56 +0430

    Register view is compeleted

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express')
+const boddyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const homeRouter = require('./routes/index')
 const userRouter = require('./routes/users')
 
 const app = express()
+
+// Bopdyparser
+app.use(boddyParser.urlencoded({extended:false}))
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+// Connect flash
+app.use(flash())
+
+// Global Vars
+app.use((req,res,next)=>{
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    
+    next()
+})
 
 app.use(express.static('public'))
 
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const db = require('./../db/db')
+const bcrypt = require('bcryptjs')
+const { v4: uuidv4 } = require('uuid')
 
 const router = express.Router()
 
@@ -10,6 +13,60 @@ router.get('/login',(req,res,next)=>{
 // Register Page
 router.get('/register',(req,res,next)=>{
     res.render('register')
+})
+
+// Register Handle
+router.post('/register',(req,res,next)=>{
+    const {username , name , email ,password} = req.body
+    let errors = []
+
+    // Check required fields
+    if(!name || !email || !password || !username){
+        errors.push({msg : 'Please fill in all fields'})
+    }
+
+    // Check Pass length
+    if(password.length < 6){
+        errors.push({msg:'Password should be at least 6 characters'})
+    }
+
+    if(errors.length > 0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            username
+        })
+    }else{
+        let sql = `SELECT * FROM users WHERE user_name = '${username}'`
+        const query1 = db.query(sql,async(err,result)=>{
+            if(err) console.error(err)
+            if(result.length === 0){
+                // Hash the password with cost 12 
+                const hashedPass = await bcrypt.hash(password, 12)
+
+                const user = {user_id: uuidv4(),user_password:hashedPass,user_name:username,name,user_email:email}
+                sql = 'INSERT INTO users SET ?'
+                const query2 = db.query(sql,user,(err,result)=>{
+                    if(err) console.error(err)
+                    console.log(result)
+                })
+                req.flash('success_msg','You are now registered and can login')
+                return res.redirect('/users/login')
+            }else{
+                // User Exists
+                errors.push({msg:'Username is already registered'})
+                return res.render('register',{
+                    errors,
+                    name,
+                    email,
+                    password,
+                    username
+                })
+            }
+        })
+    }
 })
 
 // Profile Page
