d45fb527df4caee20d698516d58bb3a3e28806d3
MoReza111 <mz.abdollahi223@gmail.com>
2021-06-05 13:41:54 +0430

    Home page is compeleted

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+
 const {ensureAuthenticated,forwardAuthenticated } = require('./../config/auth')
+const db = require('./../db')
 
 const router = express.Router()
 
@@ -8,7 +10,20 @@ router.get('/',forwardAuthenticated ,(req,res,next)=>{
 })
 
 router.get('/home',ensureAuthenticated,(req,res,next)=>{
-    res.render('home')
+    const sql = `
+        SELECT u.user_name, u.profile_image , u.user_id, p.post_id,p.content,p.created_at ,m.media_name  FROM users u
+        JOIN posts p USING (user_id)
+        LEFT JOIN posts_media pm USING (post_id)
+        LEFT JOIN media m USING(media_id)
+        JOIN followers f  ON f.following_id = u.user_id
+        WHERE f.follower_id = '${req.user.user_id}'
+    `
+    const query = db.query(sql,(err,result)=>{
+        if(err) throw err
+        console.log(req.user)
+        return res.render('home', {posts : result , user:req.user})
+    })
+    
 })
 
 module.exports = router
